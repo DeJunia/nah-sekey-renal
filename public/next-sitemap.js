ca2935b0bdd515b8ade425d416f70356a6159015
De Junia <isaactuffour744@gmail.com>
2025-04-12 14:13:03 +0000

    v1.4

---
+++ public/next-sitemap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://nahsekey-renal-care.vercel.app', // Replace with your actual domain
+  generateRobotsTxt: true, // Optional, generates robots.txt file
+  changefreq: 'daily', // Default frequency for updates
+  priority: 0.7, // Default priority for all pages
+  sitemapSize: 7000, // Optional, if you have a large number of URLs
+};
