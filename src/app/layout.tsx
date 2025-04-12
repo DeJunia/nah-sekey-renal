f37ea4f271aff5754a5bf9f08a7949e3e24be7df
De Junia <isaactuffour744@gmail.com>
2025-04-12 13:49:01 +0000

    v1.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,25 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: 'Nahsekey Renal Care',
+  metadataBase: new URL("https://nahsekey-renal-care.vercel.app"),
+  title: {
+    default: "Nah-Sekey Renal Institute",
+    template: "%s | Nah-Sekey Renal Institute",
+  },
   description: 'Nah-Sekey Renal Institute is a world-class medical center dedicated to the prevention, diagnosis, and treatment of kidney-related conditions. Led by renowned nephrologist Dr. Nah Sekey, our institute combines cutting-edge technology with compassionate care to provide personalized treatment plans for every patient. From early-stage kidney disease to advanced dialysis services, we are committed to delivering expert nephrology care in a serene and modern environment.Whether you are seeking a second opinion, ongoing treatment, or preventative consultation, Sekey Renal Institute stands as a trusted partner in your journey to optimal kidney health.',
+  keywords: [
+    "kidney clinic",
+    "renal care",
+    "kidney treatment",
+    "Nah Sekey",
+    "nephrology",
+    "dialysis center",
+    "kidney disease",
+    "kidney transplant",
+    "renal health",
+    "kidney hospital",
+    "premium kidney care",
+  ],
   openGraph: {
     title: "Nah-Sekey Renal Institute",
     description: "Premium renal healthcare by Dr. Nah Sekey.",
@@ -31,8 +48,8 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Nah-Sekey Renal Institute",
-    description: "Premium renal healthcare by Dr. Nah Sekey.",
-    images: ["../assets/images/img.jpg"],
+    description: "Premium kidney care led by Dr. Nah Sekey.",
+    images: ["../assets/images/img.jpg"], 
   },
 };
 
